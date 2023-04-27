7fcb5074652e8b610fd86b2cee73d27b05eceae5
Kur1one <bautinov.b@mail.ru>
2023-04-27 21:24:21 +0600

    main screen + description screen

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:movieapp/utils/text.dart';
+import 'package:movieapp/widgets/toprated.dart';
+import 'package:movieapp/widgets/trending.dart';
+import 'package:movieapp/widgets/tv.dart';
+import 'package:tmdb_api/tmdb_api.dart';
+
+void main()=>runApp(new MyApp());
+
+  class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        home: Home(),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark,
+        primaryColor: Colors.green),
+
+      );
+    }
+  }
+  class Home extends StatefulWidget {
+    const Home({Key key}) : super(key: key);
+  
+    @override
+    State<Home> createState() => _HomeState();
+  }
+  
+  class _HomeState extends State<Home> {
+    List trendingmovies = [];
+    List topratedmovies = [];
+    List tv = [];
+    final String apikey = '746896ecf94e873903aa47e53113a615';
+    final readaccestoken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NDY4OTZlY2Y5NGU4NzM5MDNhYTQ3ZTUzMTEzYTYxNSIsInN1YiI6IjY0NDk0MjI1NDk3NTYwMDRlNmRmNWI3NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CDCe7B5Z8Tm9fJ_iFBrxgkKq9GarPiNQXbgd4jQq07s';
+
+    @override
+    void initState() {
+      loadmovies();
+      super.initState();
+    }
+
+
+    loadmovies()async{
+TMDB tmdbWithCustomLogs = TMDB(ApiKeys(apikey, readaccestoken),
+logConfig: ConfigLogger(
+  showLogs: true,
+  showErrorLogs: true,
+));
+Map trendingresult = await tmdbWithCustomLogs.v3.trending.getTrending();
+Map topratedresult = await tmdbWithCustomLogs.v3.movies.getTopRated();
+Map tvresult = await tmdbWithCustomLogs.v3.tv.getPopular();
+setState(() {
+  trendingmovies = trendingresult['results'];
+  topratedmovies = topratedresult['results'];
+  tv = tvresult['results'];
+});
+
+print(tv);
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: modified_text(text: 'Flutter Movie App ❤️'),),
+        body: ListView(
+          children: [
+            TV(tv: tv,),
+            TopRated(toprated: topratedmovies,),
+            TrendingMovies(trending: trendingmovies)
+          ],
+        ),
+      );
+    }
+  }
+  
+
---
+++ lib/widgets/tv.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:movieapp/description.dart';
+
+import '../utils/text.dart';
+
+class TV extends StatelessWidget {
+  final List tv;
+  const TV({Key key, this.tv}) : super(key: key);
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            modified_text(text: 'Popular TV Shows', size: 26,),
+            SizedBox(height: 10,),
+            Container(height: 200,
+                child: ListView.builder(itemCount: tv.length, scrollDirection: Axis.horizontal ,itemBuilder: (context, index){
+                  return InkWell(
+                    onTap: (){
+
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>Description(name: tv[index]['original_name'],
+                        bannerurl: 'https://image.tmdb.org/t/p/w500'+tv[index]['backdrop_path'],
+                        posterurl: 'https://image.tmdb.org/t/p/w500'+tv[index]['poster_path'],
+                        description: tv[index]['overview'],
+                        vote: tv[index]['vote_average'].toString(),
+                        launch_on: tv[index]['release_date'],)));
+
+                    },
+                    child: tv[index]['original_name']!=null?Container(
+                      padding: EdgeInsets.all(5),
+                      width: 250,
+                      child: Column(
+                        children: [
+                          Container(
+
+                            width: 250,
+                            height: 140,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(
+                                image: NetworkImage(
+                                    'https://image.tmdb.org/t/p/w500' + tv[index]['backdrop_path']
+                                ), fit: BoxFit.cover
+                            )),
+                          ),
+                          SizedBox(height: 10,),
+                          Container(child: modified_text(text: tv[index]['original_name'] != null?
+                          tv[index]['original_name']:'Loading' ),)
+                        ],
+                      ),
+                    ):Container(),
+                  );
+                },))
+          ],
+        )
+    );
+  }
+}
